71a6b99dfd7e7de3757f3fec169745b46e82db5a
gestalt <verzweif@gmail.com>
2018-03-31 13:40:08 +0300

    simple read-write file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char** argv) {
-    std::cout << "spline" << std::endl;
+    std::ifstream inn;
+    std::ofstream out;
+
+    inn.open("input.txt", std::ifstream::in);
+    out.open("output.txt", std::ofstream::out);
+
+    int n = 0;
+    inn >> n;
+
+    std::cout << "n=" << n << std::endl;
+
+    //std::vector
+
+    while(!inn.eof()) {
+        float x;
+        float y ;
+        inn >> x >> y;
+
+        std::cout << "x=" << x << " y=" << y << std::endl;
+        out << x << " " << y << std::endl;
+    }
+
+    inn.close();
+    out.close();
+
+    std::cout << std::endl;
 
 	return 0;
 }
