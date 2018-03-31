640d9323ca7ef0d7d96c875fcc175fbdad209bc1
gestalt <verzweif@gmail.com>
2018-03-31 13:35:11 +0300

    init

---
+++ build.bat
@@ -0,0 +1,9 @@
+cls
+if exist build rmdir /S /Q build
+mkdir build
+cd build
+
+cmake -G "Borland Makefiles" ../ || exit /b
+make || exit /b
+
+spline
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+    std::cout << "spline" << std::endl;
+
+	return 0;
+}
