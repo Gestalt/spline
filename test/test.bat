764fcb0a1e12d9cc50127a56c6660072bd4ca7cc
gestalt <verzweif@gmail.com>
2018-04-04 23:18:37 +0300

    repository review

--- test/test.bat
+++ test/test.bat
@@ -3,7 +3,7 @@ if exist build rmdir /S /Q build
 mkdir build
 cd build
 
-cmake -G "Visual Studio 8 2005" ../ || exit /b
+cmake -G "Visual Studio 8 2005" ../ -DUSE_BOOST:BOOL=ON || exit /b
 
 cmake --build . --config Release || exit /b
 ctest --verbose --output-on-failure
